89bf4a3a2673aad0ecc814a8a30071ec28c654b2
ark12321 <ar942025@student.nitw.ac.in>
2024-02-01 23:34:45 +0530

    initial commit

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const date = require(__dirname + "/date.js");
 const mongoose = require("mongoose");
 const _ = require("lodash");
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
 
@@ -156,7 +157,7 @@ app.get("/about", function (req, res) {
   res.render("about");
 });
 
-app.listen(3000, function () {
+app.listen(PORT, function () {
   console.log("Server started on port 3000");
 });
 
